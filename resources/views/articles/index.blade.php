870a118f01621ce3669e488c9466c012eabe73a7
unknown <dullstart.yang.com>
2024-01-29 19:13:34 +0800

    css && vite

---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.article')
+
+@section('main')
+<h1 class="m-2">文章列表</h1>
+
+@endsection
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+module.exports = {
+    content: [
+        "./src/**/*.{html,js}",
+        "./resources/**/*.css",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
